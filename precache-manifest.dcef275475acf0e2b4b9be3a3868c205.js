a5ab41f83bb332d4a77102847b79cb8c5bb3eab2
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-30 12:17:00 +0300

    Updates

---
+++ precache-manifest.dcef275475acf0e2b4b9be3a3868c205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7940faba466acad9b7bbb99b4cb114a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c12e71c4c1afe8ade93",
+    "url": "./static/css/main.9d969998.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c12e71c4c1afe8ade93",
+    "url": "./static/js/main.db604342.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
